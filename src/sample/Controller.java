4a6883d158e70fe115d7048acd40977325f64283
Patrick <pwblais@icloud.com>
2017-05-08 20:56:32 -0400

    Merge branch 'master' of C:\Users\pwbla\IdeaProjects\ICS3U-Final with conflicts.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Random;
